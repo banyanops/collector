b3f9ce0ab503e952caf02f71fd4c1f2e39d3c486
Yoshio Turner <yoshio@banyanops.com>
2015-05-29 11:12:46 -0700

    Simplified.

--- data/defaultscripts/pkgextractscript.sh
+++ data/defaultscripts/pkgextractscript.sh
@@ -34,7 +34,10 @@ then
 	for line in $(dpkg-query -W -f '${Package}\t${Version}\t${Architecture}\n'); do
 		echo $line | busybox awk '{printf "- pkg: \"%s\"\n  version: \"%s\"\n  architecture: \"%s\"\n", $1, $2, $3}'
 	done
-elif [ -f /etc/redhat-release ]
+	exit 0
+fi
+
+if [ -f /etc/redhat-release ]
 then
 	a=`busybox which rpm`
 	if [ -z $a ]
@@ -42,25 +45,12 @@ then
 		exit 1
 	fi
 	# run rpm in the context of the container under inspection
+	IFS=$'\n'
 	echo "pkgsinfo:"
-	for line in $(rpm -qa); do
-		c=( ${line//-/ } )  # array with elements determined by hyphen separator
-		n=${#c[@]} # length of array c
-		if [ "$n" -ge 3 ]; then
-			relarch=${c[$n-1]}  # release.arch
-			arr=( ${relarch//./ } )
-			arch=${arr[${#arr[@]}-1]} # field after last "." (period character)
-			subs=".${arch}"
-			release=${relarch%$subs} # delete arch from relarch
-			version=${c[$n-2]}  # package version
-			subs="-${version}-${relarch}" # trailing substring to remove to get the package name
-			version="${version}-${release}" # combine package version and release together
-			name=${line%$subs}  # get package name by removing trailing substring
-			echo "- pkg: \"${name}\""
-			echo "  version: \"${version}\""
-			echo "  architecture: \"${arch}\""
-		fi
+	for line in $(rpm -qa --qf '%{n}\t%{v}-%{r}\t%{arch}\n'); do
+		echo $line | busybox awk '{printf "- pkg: \"%s\"\n  version: \"%s\"\n  architecture: \"%s\"\n", $1, $2, $3}'
 	done
-else
-	exit 2
+	exit 0
 fi
+
+exit 2
